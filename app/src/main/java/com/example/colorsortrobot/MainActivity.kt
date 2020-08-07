122bcafc7869226a1d7ee43e31a0a57b741b92a1
Omar Sabri <oz9.sa7@gmail.com>
2020-08-07 17:37:43 +0300

    ✅ Ask user to enable bluetooth if it was disabled

--- app/src/main/java/com/example/colorsortrobot/MainActivity.kt
+++ app/src/main/java/com/example/colorsortrobot/MainActivity.kt
@@ -1,46 +1,64 @@
 package com.example.colorsortrobot
 
-import android.app.Dialog
 import android.os.Bundle
-import android.view.Window
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.colorsortrobot.ViewModels.BluetoothViewModel
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: BluetoothViewModel
+    private lateinit var loadingDialog: android.app.AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        init()
+        observeChanges()
+
+        viewModel.checkBluetoothConnection()
+        loadingDialog.show()
+    }
+
+    private fun init() {
         // init viewModel
         viewModel = ViewModelProvider(this).get(BluetoothViewModel::class.java)
 
-        observeChanges()
-        viewModel.checkBluetoothConnection()
+        // init loading dialog
+        loadingDialog = SpotsDialog.Builder().setContext(this).build()
     }
 
     private fun observeChanges() {
         // Observe for bluetooth status changed
         viewModel.getStatusObserver().observe(this, Observer {
+            loadingDialog.hide()
             when (it) {
                 true -> viewModel.getList() // todo: get list of connected devices, highlight the one connected to last time
-                false -> showEnableBluetoothDialog()
-                null -> showEnableBluetoothDialog() // todo not supported dialog
+                false -> showEnableBluetoothMessage()
+                null -> showEnableBluetoothMessage() // todo not supported dialog
             }
         })
     }
 
-    private fun showEnableBluetoothDialog() {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.enable_bluetooth_dialog)
-        // val body = dialog.findViewById(R.id.body) as TextView
-        dialog.show()
+    private fun hideStuff() {
+        loadingDialog.hide()
+        enableBluetooth.visibility = View.GONE
+    }
+
+    private fun showEnableBluetoothMessage() {
+        hideStuff()
+        enableBluetooth.visibility = View.VISIBLE
+        enableBluetooth.findViewById<Button>(R.id.button).setOnClickListener {
+
+        }
+
     }
 
 }
